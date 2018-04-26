fe7ca4f1294e347539310eae3ac078e404e28d21
Mesburis, Cassie <cmesburis@lcs-mba3252.lcs.on.ca>
2018-04-26 18:49:25 -0400

    done

--- ChipsFastFoodEmporium/main.swift
+++ ChipsFastFoodEmporium/main.swift
@@ -9,13 +9,82 @@
 import Foundation
 
 // INPUT
-// Collect and filter user input here
+func getMealChoice(withPrompt: String) -> Int {
+    
+    var choice = 0
+    while 1 == 1 {
+        
+        // Prompt the user
+        print(withPrompt, terminator: "")
+        
+        // Test #1: Get input and make sure it is not nil
+        guard let givenInput = readLine() else {
+            
+            // Prompt again when input is nil
+            continue
+        }
+        
+        // Test #2: Can it be an integer?
+        guard let givenInteger = Int(givenInput) else {
+            
+            // Prompt again when input cannot be made into an integer
+            continue
+        }
+        
+        // Test #3: Is it in the acceptable range of 1 to 4?
+        if givenInteger < 1 || givenInteger > 4 {
+            
+            // Prompt again when too low or too hight
+            continue
+        }
+        
+        // If we got here, input is valid
+        choice = givenInteger
+        break
+        
+    }
+    
+    // Exit the function
+    return choice
+}
 
+// Get the burger choice
+let burgerChoice = getMealChoice(withPrompt: "Please enter a burger choice: ")
+let sideChoice = getMealChoice(withPrompt: "Please enter a side order choice: ")
+let drinkChoice = getMealChoice(withPrompt: "Please enter a drink choice: ")
+let dessertChoice = getMealChoice(withPrompt: "Please enter a dessert choice: ")
 
 // PROCESS
-// Implement the primary logic of the problem here
 
+var burgerCalorieCounts = Array(repeating: 0, count: 5)
+burgerCalorieCounts[1] = 461 // Cheeseburger
+burgerCalorieCounts[2] = 431 // Fish Burger
+burgerCalorieCounts[3] = 420 // Veggie Burger
+burgerCalorieCounts[4] = 0   // Nothing
+
+var sideOrderCalorieCounts = Array(repeating: 0, count: 5)
+sideOrderCalorieCounts[1] = 100 // Fries
+sideOrderCalorieCounts[2] = 57  // Baked potato
+sideOrderCalorieCounts[3] = 70  // Chef salad
+sideOrderCalorieCounts[4] = 0   // Nothing
+
+var drinkCaloriesCounts = Array(repeating: 0, count: 5)
+drinkCaloriesCounts[1] = 130 // Soft drink
+drinkCaloriesCounts[2] = 160 // Orange juice
+drinkCaloriesCounts[3] = 118 // Milk
+drinkCaloriesCounts[4] = 0   // Nothing
+
+var dessertCalorieCounts = Array(repeating: 0, count: 5)
+dessertCalorieCounts[1] = 167   // Apple pie
+dessertCalorieCounts[2] = 266   // Sundae
+dessertCalorieCounts[3] = 75    // Fruit cup
+dessertCalorieCounts[4] = 0     // Nothing
+
+// Calculate calories amount
+let totalCalories = burgerCalorieCounts[burgerChoice] +
+    sideOrderCalorieCounts[sideChoice] +
+    drinkCaloriesCounts[drinkChoice] +
+    dessertCalorieCounts[dessertChoice]
 
 // OUTPUT
-// Report results to the user here
-
+print("Your total Calorie count is \(totalCalories).")
